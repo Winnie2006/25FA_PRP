dbaa616466292b1d940fb8dacfad4d55730b20e2
Yunqi Ji <jiyunqi2024@sjtu.edu.cn>
2025-09-25 23:17:03 +0800

    change input type to DICOM

--- label.py
+++ label.py
@@ -1,3 +1,5 @@
+import pydicom
+import numpy as np
 import os
 import cv2
 import tkinter as tk
@@ -5,16 +7,16 @@ from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
 from openpyxl import Workbook, load_workbook
 
-class VideoViewer:
+class DicomViewer:
     def __init__(self, root, folder_path, output_file_path):
         self.root = root
-        self.root.title("视频查看器")
+        self.root.title("DICOM查看器")
         self.folder_path = folder_path
-        self.videos = self.get_all_videos(folder_path)
+        self.dicom_paths = self.get_all_dicoms(folder_path)
         self.output_file_path = output_file_path
 
-        if not self.videos:
-            raise ValueError("指定的文件夹中没有视频文件")
+        if not self.dicom_paths:
+            raise ValueError("指定的文件夹中没DICOM文件")
 
         # 检查 Excel 文件是否存在
         if not os.path.exists(self.output_file_path):
@@ -23,66 +25,120 @@ class VideoViewer:
         # 从 Excel 文件中获取当前未分类的视频索引
         self.current_index = self.find_first_unclassified_video()
 
-        self.video_path = self.videos[self.current_index]
-        self.cap = cv2.VideoCapture(self.video_path)
+        # self.video_path = self.videos[self.current_index]
+        # self.cap = cv2.VideoCapture(self.video_path)
+        # 改为读取所有DICOM文件并存储为图像列表
+        self.images = []
+        for p in self.dicom_paths:
+            arr = self.load_dicom_image(p)
+            if arr is not None:
+                self.images.extend(arr)  # 展开成单帧列表
+
+        if not self.images:
+            raise ValueError("没有可用的视频帧 (所有文件都可能是截图/频谱)")
 
         self.label = tk.Label(root)
         self.label.pack()
 
-        self.root.bind("<Left>", self.show_previous_video)
-        self.root.bind("<Right>", self.show_next_video)
-        self.root.bind("<KP_Left>", self.show_previous_video)  # 左方向键
-        self.root.bind("<KP_Right>", self.show_next_video)  # 右方向键
-        self.root.bind("<space>", self.toggle_pause)  # 按空格键暂停或继续
+        self.root.bind("<Left>", self.show_previous_frame)
+        self.root.bind("<Right>", self.show_next_frame)
+        self.root.bind("<KP_Left>", self.show_previous_frame)  # 左方向键
+        self.root.bind("<KP_Right>", self.show_next_frame)  # 右方向键
+        # self.root.bind("<space>", self.toggle_pause)  # 按空格键暂停或继续(现在不是自动播放，故不需要)
 
-        self.paused = False
-        self.update_frame()
+        # self.paused = False
+        # self.update_frame()
+        self.show_frame()
 
         # 创建分类标注窗口
         self.create_classification_window()
 
-    def get_all_videos(self, folder_path):
-        videos = []
+    # def get_all_videos(self, folder_path):
+    #     videos = []
+    #     for subdir, dirs, files in os.walk(folder_path):
+    #         for filename in files:
+    #             if filename.lower().endswith(('.mp4', '.avi', '.mov', '.mkv', '.flv')):
+    #                 videos.append(os.path.join(subdir, filename))
+    #     return videos
+
+    def get_all_dicoms(self, folder_path):
+        dicoms = []
         for subdir, dirs, files in os.walk(folder_path):
             for filename in files:
-                if filename.lower().endswith(('.mp4', '.avi', '.mov', '.mkv', '.flv')):
-                    videos.append(os.path.join(subdir, filename))
-        return videos
+                if filename.lower().endswith(".dcm"):
+                    dicoms.append(os.path.join(subdir, filename))
+        return dicoms
 
-    def update_frame(self):
-        if not self.paused:
-            ret, frame = self.cap.read()
-            if ret:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                img = Image.fromarray(frame)
-                photo = ImageTk.PhotoImage(image=img)
-                self.label.config(image=photo)
-                self.label.image = photo
-                self.root.after(30, self.update_frame)  # 每30ms更新一次
-            else:
-                self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # 重置视频到开头
-                self.update_frame()
+    def load_dicom_image(self, path):
+        ds = pydicom.dcmread(path)
+        pixel_array = ds.pixel_array
 
-    def show_previous_video(self, event=None):
-        self.cap.release()
-        self.current_index = (self.current_index - 1) % len(self.videos)
-        self.video_path = self.videos[self.current_index]
-        self.cap = cv2.VideoCapture(self.video_path)
-        self.paused = False
-        self.update_frame()
+        # 判断是否是视频帧 (multi-frame)
+        if pixel_array.ndim == 3 and pixel_array.shape[0] > 1:
+            frames = []
+            for frame in pixel_array:
+                frame = self.normalize_image(frame)
+                frames.append(frame)
+            return frames
+        else:
+            # 单帧 -> 认为是截图/频谱，丢弃
+            return None
+    
+    def normalize_image(self, img):
+        img = img.astype(np.float32)
+        img = 255 * (img - np.min(img)) / (np.max(img) - np.min(img) + 1e-5)
+        return img.astype(np.uint8)
 
-    def show_next_video(self, event=None):
-        self.cap.release()
-        self.current_index = (self.current_index + 1) % len(self.videos)
-        self.video_path = self.videos[self.current_index]
-        self.cap = cv2.VideoCapture(self.video_path)
-        self.paused = False
-        self.update_frame()
+    def show_frame(self):
+        if 0 <= self.current_index < len(self.images):
+            frame = self.images[self.current_index]
+            img = Image.fromarray(frame).convert("L")  # 灰度图
+            photo = ImageTk.PhotoImage(image=img)
+            self.label.config(image=photo)
+            self.label.image = photo
+    
+    def show_previous_frame(self, event=None):
+        self.current_index = (self.current_index - 1) % len(self.images)
+        self.show_frame()
 
-    def toggle_pause(self, event=None):
-        self.paused = not self.paused
-        if not self.paused:
-            self.update_frame()
+    def show_next_frame(self, event=None):
+        self.current_index = (self.current_index + 1) % len(self.images)
+        self.show_frame()
+
+    # def update_frame(self):
+    #     if not self.paused:
+    #         ret, frame = self.cap.read()
+    #         if ret:
+    #             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #             img = Image.fromarray(frame)
+    #             photo = ImageTk.PhotoImage(image=img)
+    #             self.label.config(image=photo)
+    #             self.label.image = photo
+    #             self.root.after(30, self.update_frame)  # 每30ms更新一次
+    #         else:
+    #             self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # 重置视频到开头
+    #             self.update_frame()
+
+    # def show_previous_video(self, event=None):
+    #     self.cap.release()
+    #     self.current_index = (self.current_index - 1) % len(self.videos)
+    #     self.video_path = self.videos[self.current_index]
+    #     self.cap = cv2.VideoCapture(self.video_path)
+    #     self.paused = False
+    #     self.update_frame()
+
+    # def show_next_video(self, event=None):
+    #     self.cap.release()
+    #     self.current_index = (self.current_index + 1) % len(self.videos)
+    #     self.video_path = self.videos[self.current_index]
+    #     self.cap = cv2.VideoCapture(self.video_path)
+    #     self.paused = False
+    #     self.update_frame()
+
+    # def toggle_pause(self, event=None):
+    #     self.paused = not self.paused
+    #     if not self.paused:
+    #         self.update_frame()
 
     def close(self):
         self.cap.release()
@@ -91,15 +147,15 @@ class VideoViewer:
     def create_excel_file(self):
         wb = Workbook()
         ws = wb.active
-        ws.title = "Video Names"
-        ws.append(["视频文件名", "分类"])
+        ws.title = "Dicom Names"
+        ws.append(["文件名", "分类"])
 
-        for video_path in self.videos:
-            video_name = os.path.basename(video_path)
-            ws.append([video_name, "未分类"])
+        for dicom_path in self.dicom_paths:
+            dicom_name = os.path.basename(dicom_path)
+            ws.append([dicom_name, "未分类"])
 
         wb.save(self.output_file_path)
-        print(f"Excel 文件已创建并保存到桌面: {self.output_file_path}")
+        print(f"Excel 文件已创建并保存: {self.output_file_path}")
 
     def find_first_unclassified_video(self):
         wb = load_workbook(self.output_file_path)
@@ -114,7 +170,7 @@ class VideoViewer:
         wb = load_workbook(self.output_file_path)
         ws = wb.active
 
-        for i in range(self.current_index + 1, len(self.videos) + 1):
+        for i in range(self.current_index + 1, len(self.dicom_paths) + 1):
             row = ws.cell(row=i + 1, column=2).value
             if row == "未分类":
                 return i - 1  # 返回未分类视频的索引
@@ -203,8 +259,8 @@ class VideoViewer:
         self.delete_button = tk.Button(self.classification_window, text="删除分类", command=self.delete_classification)
         self.delete_button.pack()
 
-    def classify_video(self):
-        video_name = os.path.basename(self.videos[self.current_index])
+    def classify_dicom(self):
+        dicom_name = os.path.basename(self.dicom_paths[self.current_index])
         classification = self.classification_var.get()
 
         if not classification:
@@ -215,12 +271,12 @@ class VideoViewer:
         ws = wb.active
 
         for row in ws.iter_rows(min_row=2, max_col=2, values_only=False):
-            if row[0].value == video_name:
+            if row[0].value == dicom_name:
                 row[1].value = classification
                 break
 
         wb.save(self.output_file_path)
-        print(f"视频 {video_name} 分类为 {classification}，已保存到 Excel 文件。")
+        print(f"视频 {dicom_name} 分类为 {classification}，已保存到 Excel 文件。")
 
         # 自动切换到下一个未分类的视频
         self.current_index = self.find_next_unclassified_video()
@@ -276,16 +332,16 @@ class VideoViewer:
 desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
 
 # 指定视频所在的根目录（假设视频文件夹也在桌面上）
-videos_folder_name = "videos"  # 视频文件夹的名称
-videos_folder_path = os.path.join(desktop_path, videos_folder_name)
+dicom_folder_name = "dicoms"  # 视频文件夹的名称
+dicom_folder_path = os.path.join(desktop_path, dicom_folder_name)
 
 # 指定保存分类结果的 Excel 文件路径
-excel_file_name = "video_names.xlsx"
+excel_file_name = "dicom_names.xlsx"
 output_file_path = os.path.join(desktop_path, excel_file_name)
 
 # 创建主窗口
 root = tk.Tk()
-app = VideoViewer(root, videos_folder_path, output_file_path)
+app = DicomViewer(root, dicom_folder_path, output_file_path)
 
 # 运行主循环
 root.mainloop()
